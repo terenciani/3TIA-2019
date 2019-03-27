722856163fa703917a60c5c77b7cabf621ef7904
Aluno <aluno@aluno.com>
2019-03-27 10:16:18 -0300

    Formulário de login

---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-layout align-center justify-center row fill-height>
+	    <v-flex xs4>
+			<v-card class="pa-2">
+				<v-text-field label="E-mail" outline/>
+				<v-text-field label="Senha" outline/>
+				<h1>Teste</h1>
+				<v-btn color="info" outline>Login</v-btn>
+			</v-card>
+		</v-flex>
+	</v-layout>
+</template>
